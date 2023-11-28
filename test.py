bc79398f0d4aaf95742e750a1dd6c05a2c5cbc81
kofismartgh <90314355+kofismartgh@users.noreply.github.com>
2023-11-28 10:41:48 +0000

    Update test.py King added

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World! From Green Team'
+    return 'Hello, KING! From Green Team'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
